0fbc23469ffb63e4f61eaff05b0698fd631708c5
de160637 <phatbvgde160637@fpt.edu.vn>
2023-05-31 09:40:35 +0700

    lab 1-2-3-4

---
+++ Js/scripts.js
@@ -0,0 +1,27 @@
+
+    $(document).ready(function () {
+        $('[data-toggle="tooltip"]').tooltip();
+    });
+
+    $("#carouselButton").click(function(){
+                 if ($("#carouselButton").children("span").hasClass('fa-pause')) {
+                     $("#mycarousel").carousel('pause');
+                     $("#carouselButton").children("span").removeClass('fa-pause');
+                     $("#carouselButton").children("span").addClass('fa-play');
+                 }
+                 else if ($("#carouselButton").children("span").hasClass('fa-play')){
+                     $("#mycarousel").carousel('cycle');
+                     $("#carouselButton").children("span").removeClass('fa-play');
+                     $("#carouselButton").children("span").addClass('fa-pause');                    
+                 }
+ });
+
+ $('#reverse-button').click(function (){
+    $('#reverse-card').modal('toggle');
+  });
+//   Week4 Assignment Task3
+  $('#login-button').click(function (){
+    $('#loginModal').modal('toggle');
+  });
+ 
+ 
